a7d21ba9549571b21cc942cf4ad1873fa075e49e
compilerharris <haris@technosive.co.uk>
2024-02-28 23:18:40 +0530

    remove space

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
-
 @Module({
     imports: [NestConfigModule.forRoot({
         validationSchema: Joi.object({
