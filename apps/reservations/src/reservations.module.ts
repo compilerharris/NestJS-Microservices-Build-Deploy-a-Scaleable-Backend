9eb8e8fd2b90b5776055bf91a33d18c80039919c
compilerharris <haris@technosive.co.uk>
2024-03-04 16:02:30 +0530

    Validation & Logging

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,4 +1,4 @@
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { ReservationsController } from './reservations.controller';
@@ -6,7 +6,11 @@ import { ReservationsRepository } from './reservations.repository';
 import { ReservationsService } from './reservations.service';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}]),
+    LoggerModule
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
